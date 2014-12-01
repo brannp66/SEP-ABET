238c8f6b694f2eec163a10373c77be20f9c15311
mithranor <mithranor@gmail.com>
2014-11-30 20:06:06 -0600

    Added stuff

---
+++ admin_page.py
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title>ABET Accreddidation</title>
+  </head>
+  <body>
+    <h1>Welcome User!</h1>
+    <a href="logout.py">Log Out</a>
+  </body>
---
+++ log_in.php
@@ -0,0 +1,45 @@
+<!--  -->
+
+
+<?php
+
+    session_start();
+
+    if(isset($_POST['signIn'])){
+         $_SESSION['email'] = $_POST['email'];
+         $_SESSION['password'] = $_POST['password'];
+
+         header("Location: user_page.php");
+    }
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title>ABET Accreddidation</title>
+    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+    <script type="text/javascript" src='js/IndexJS.js'></script>
+  </head>
+
+  <body>
+    <div class = "pageTitle">
+      <h1>ABET Tallys</h1>
+    </div>
+    <div id="loginContainer">
+      <form id="login" action = "validateUserLogin.php" method = "post">
+        <div class="logEmail">
+            <label>Username:</label>
+            <input type="username" name = "username" id="username"/>
+        </div>
+        <div class="logPass"> 
+            <label>Password:</label>
+            <input type="password" id="logPass" name = "password"/> 
+        </div>
+        <div class = "loginButton">
+          <input type="submit" value="Login" name="signIn" id="signIn" />
+        </div>
+      </form>
+    </div> 
+  </body>
+</html>
