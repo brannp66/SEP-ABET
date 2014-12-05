0736229493eb65b1da216997172b5b21875a4cf4
brannp66 <brannp66@gmail.com>
2014-12-04 20:48:56 -0600

    disable buttons on admin.php

--- admin.php
+++ admin.php
@@ -10,49 +10,64 @@ include 'phpAPI.php';
     <link rel="stylesheet" href="css/foundation.css" />
     <script src="js/vendor/modernizr.js"></script>
     <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+    <script>
+    	function toggleButton(str) {
+    		var elements = document.getElementsByClassName("btn");
+    		for(var i = 0; i < elements.length; i++){
+					if(str=="") {
+						elements[i].disabled = true;
+					}
+					else
+					{
+						elements[i].disabled = false;
+					}
+				}
+			}
+		</script>
   </head>
+
 	<body>
-		<div class='choose'>				<div class='dropdown'>
-					<select name = 'semester' onchange="loadCourses(this.value)">
-						<option value="" disabled selected style='display:none;'>Select Semester</option>
-						<option value="Fall2014">Fall 2014</option>
-						<option value="Spring2015">Spring 2015</option>
-						<option value="Fall2015">Fall 2015</option>
-						<option value="Spring2016">Spring 2016</option>
-						<option value="Fall2016">Fall 2016</option>
-						<option value="Spring2017">Spring 2017</option>
-						<option value="Fall2017">Fall 2017</option>
-					</select>
-				</div>
-				<div class='dropdown' id='courses'>
-				</div>
+		<div class='choose'>				
+			<div class='dropdown'>
+				<select name = 'semester' onchange="toggleButton(this.value)">
+					<option value="" disabled selected style='display:none;'>Select Semester</option>
+					<option value="Fall2014">Fall 2014</option>
+					<option value="Spring2015">Spring 2015</option>
+					<option value="Fall2015">Fall 2015</option>
+					<option value="Spring2016">Spring 2016</option>
+					<option value="Fall2016">Fall 2016</option>
+					<option value="Spring2017">Spring 2017</option>
+					<option value="Fall2017">Fall 2017</option>
+				</select>
+			</div>
 		</div>
 
 		<div id = "OutcomeForms">
 			<form action='stats.php' method = 'POST'>
-				<button type="submit" value = 'cac=a'>CAC-A</button>
-				<button type="submit" value = 'cac=b'>CAC-B</button>
-				<button type="submit" value = 'cac=c'>CAC-C</button>
-				<button type="submit" value = 'cac=d'>CAC-D</button>
-				<button type="submit" value = 'cac=e'>CAC-E</button>
-				<button type="submit" value = 'cac=f'>CAC-F</button>
-				<button type="submit" value = 'cac=g'>CAC-G</button>
-				<button type="submit" value = 'cac=h'>CAC-H</button>
-				<button type="submit" value = 'cac=i'>CAC-I</button>
-				<button type="submit" value = 'cac=j'>CAC-J</button>
-				<button type="submit" value = 'cac=k'>CAC-K</button>
-
-				<button type="submit" value = 'eac=a'>EAC-A</button>
-				<button type="submit" value = 'eac=b'>EAC-B</button>
-				<button type="submit" value = 'eac=c'>EAC-C</button>
-				<button type="submit" value = 'eac=d'>EAC-D</button>
-				<button type="submit" value = 'eac=e'>EAC-E</button>
-				<button type="submit" value = 'eac=f'>EAC-F</button>
-				<button type="submit" value = 'eac=g'>EAC-G</button>
-				<button type="submit" value = 'eac=h'>EAC-H</button>
-				<button type="submit" value = 'eac=i'>EAC-I</button>
-				<button type="submit" value = 'eac=j'>EAC-J</button>
-				<button type="submit" value = 'eac=k'>EAC-K</button>
+				<h1>CAC Results</h1>
+				<button type="submit" value = 'cac=a' name='form' class='btn' disabled>CAC-A</button>
+				<button type="submit" value = 'cac=b' name='form' class='btn' disabled>CAC-B</button>
+				<button type="submit" value = 'cac=c' name='form' class='btn' disabled>CAC-C</button>
+				<button type="submit" value = 'cac=d' name='form' class='btn' disabled>CAC-D</button>
+				<button type="submit" value = 'cac=e' name='form' class='btn' disabled>CAC-E</button>
+				<button type="submit" value = 'cac=f' name='form' class='btn' disabled>CAC-F</button>
+				<button type="submit" value = 'cac=g' name='form' class='btn' disabled>CAC-G</button>
+				<button type="submit" value = 'cac=h' name='form' class='btn' disabled>CAC-H</button>
+				<button type="submit" value = 'cac=i' name='form' class='btn' disabled>CAC-I</button>
+				<button type="submit" value = 'cac=j' name='form' class='btn' disabled>CAC-J</button>
+				<button type="submit" value = 'cac=k' name='form' class='btn' disabled>CAC-K</button>
+				<h1>EAC Results</h1>
+				<button type="submit" value = 'eac=a' name='form' class='btn' disabled>EAC-A</button>
+				<button type="submit" value = 'eac=b' name='form' class='btn' disabled>EAC-B</button>
+				<button type="submit" value = 'eac=c' name='form' class='btn' disabled>EAC-C</button>
+				<button type="submit" value = 'eac=d' name='form' class='btn' disabled>EAC-D</button>
+				<button type="submit" value = 'eac=e' name='form' class='btn' disabled>EAC-E</button>
+				<button type="submit" value = 'eac=f' name='form' class='btn' disabled>EAC-F</button>
+				<button type="submit" value = 'eac=g' name='form' class='btn' disabled>EAC-G</button>
+				<button type="submit" value = 'eac=h' name='form' class='btn' disabled>EAC-H</button>
+				<button type="submit" value = 'eac=i' name='form' class='btn' disabled>EAC-I</button>
+				<button type="submit" value = 'eac=j' name='form' class='btn' disabled>EAC-J</button>
+				<button type="submit" value = 'eac=k' name='form' class='btn' disabled>EAC-K</button>
 
 			</form>
 		</div>
